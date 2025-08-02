bd2d1d047c3a12e8529e5383327a71f8bf77fac7
Raahul423 <rpal24480@gmail.com>
2025-08-02 15:00:27 +0530

    some edit

--- src/assets/files.js
+++ src/assets/files.js
@@ -1,14 +1,14 @@
 import thumbnail1 from './Thumbnails/VlPiVmYuoqw-HD.jpg'
 import thumbnail2 from './Thumbnails/Screenshot 2024-09-14 010532.png'
 
-import icon1 from './Shorts Images/youtubeicon.svg'
-import icon2 from './Shorts Images/shorts.svg'
-import icon3 from './Shorts Images/circleshorts.svg'
-import icon4 from './Shorts Images/youtube.svg'
-import icon5 from './Shorts Images/setting.svg'
-import icon6 from './Shorts Images/flag.svg'
-import icon7 from './Shorts Images/question mark.svg'
-import icon8 from './Shorts Images/feedback.svg'
+import icon1 from './Shorts images/youtubeicon.svg'
+import icon2 from './Shorts images/shorts.svg'
+import icon3 from './Shorts images/circleshorts.svg'
+import icon4 from './Shorts images/youtube.svg'
+import icon5 from './Shorts images/setting.svg'
+import icon6 from './Shorts images/flag.svg'
+import icon7 from './Shorts images/question mark.svg'
+import icon8 from './Shorts images/feedback.svg'
 import menu from '../assets/menu_34dp_E8EAED_FILL0_wght400_GRAD0_opsz40.png'
 
 export const Images = {
